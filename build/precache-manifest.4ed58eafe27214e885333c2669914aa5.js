01978a7016110dbf314c81b3341bd432c373aa8d
Your Name <you@example.com>
2021-11-07 12:25:15 +0530

    second commit

---
+++ build/precache-manifest.4ed58eafe27214e885333c2669914aa5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b284ae5a75bfbc9543e80cd0b346b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f688d5f2256f64470d59",
+    "url": "/static/js/2.aa89413b.chunk.js"
+  },
+  {
+    "revision": "4b56d93c478f64733ad0",
+    "url": "/static/js/main.1fb37fd3.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
